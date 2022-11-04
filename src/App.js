d296c8929b13982c9403b52ec0270f4c7b4449bf
shaahfardin <fardinshaah@gmail.com>
2022-11-05 01:01:30 +0600

    Flowbite added for Tailwind components library

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1 className='text-5xl text-red-700'>Hello World!</h1>
+
+      <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Purple to Blue</button>
+
     </div>
   );
 }
